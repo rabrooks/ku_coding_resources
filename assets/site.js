7481bfd1b853f2212337fc44b0daf0c5e7792f49
Aaron Brooks <aaronbrooks322@gmail.com>
2017-09-21 12:56:48 -0500

    Add javascript for menu

---
+++ assets/site.js
@@ -0,0 +1,22 @@
+var SideNav = (function(){
+	var navItem = $(".nav-item a");
+
+	function init() {
+		$(navItem).on('click', function() {
+			$(".nav-item .active").removeClass("active");
+			$(this).addClass("active")
+		});
+	}
+	return {
+		init: init
+	}
+})();
+
+
+
+
+$(document).ready(function() {
+	SideNav.init();
+});
+
+
